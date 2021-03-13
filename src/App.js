4a54120e47c8bccfa22b923853d499f93dc6e898
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-03-13 00:22:45 -0300

    Incorporaçao dos Componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Principal from './components/Principal';
+import Rodape from './components/Rodape';
+import Topo from './components/Topo';
+import './index.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+    <Topo />
+    <Principal />
+    <Rodape />
+  </>
+  )
+
 }
 
 export default App;
---
+++ src/components/Rodape.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Rodape = () => {
+  return (
+    <footer className="rodape">
+      <p>
+        Conteúdos cedidos pela Editora NovaTec &#8212; &copy; Copyrigth 2020
+      </p>
+    </footer>
+  )
+}
+
+export default Rodape
